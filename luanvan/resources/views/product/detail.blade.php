fde446b19c5b5b4842dae0cc34c9f85e5f6cb92b
Q.Son <quocson2521@gmail.com>
2021-10-29 05:38:43 +0700

    das

---
+++ luanvan/resources/views/product/detail.blade.php
@@ -0,0 +1,20 @@
+@extends('home.index')
+@section('content')
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="container-inner">
+                    <ul>
+                        <li class="home">
+                            <a href="index.html">Home</a>
+                            <span><i class="fa fa-angle-right"></i></span>
+                        </li>
+                        <li class="category3"><span>Shop List</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- luanvan/routes/web.php
+++ luanvan/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
+});
+Route::get('/product-details', function () {
+    return view('product.detail');
 });
